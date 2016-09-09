2495ae0bb4e92ec482c02d956440861b05b718fc
Trevor Park <ifndefdeadmau5@gmail.com>
2016-09-09 17:40:03 +0900

    Add section at Activity page
    - Create a new directory for Activity

---
+++ src/components/Activity/Activity.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Navigator,
+  Image,
+  ListView,
+  ScrollView,
+} from 'react-native';
+import Row from './Row';
+import ServerUtil from '../../utils/ServerUtil'
+
+class Activity extends Component {
+  constructor(props) {
+    super(props);
+
+    ServerUtil.initCallback(this.onRequestSuccess, this.onRequestFail);
+  }
+
+  getInitialStates() {
+      return {
+        dataBlob: {},
+        dataSource: new ListView.DataSource({
+          rowHasChanged: (r1, r2) => r1 !== r2,
+          sectionHeaderHasChanged: (s1, s2) => s1 !== s2
+        }),
+        loaded: false
+      };
+  }
+
+  onRequestSuccess(result) {
+    console.log(result);
+  }
+
+  onRequestFail(error) {
+    console.log(error);
+  }
+
+  componentWillMount() {
+    this.setState(this.getInitialStates);
+  }
+
+  componentDidMount() {
+    this.setState({
+
+      // TODO: Replace with server data
+      dataSource : this.state.dataSource.cloneWithRowsAndSections({ s1: [1,2,3,4], s2: [3,4] }, ['s1', 's2']),
+      loaded     : true
+    });
+
+    ServerUtil.getActivityList();
+  }
+
+  renderRow(rowData) {
+    return <Row />;
+  }
+
+  renderSectionHeader(sectionData, sectionID) {
+    return (
+      <View style={styles.section}>
+        <Text style={styles.text}>{sectionID}</Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <ListView
+        style = {{marginTop: 50}}
+        dataSource = {this.state.dataSource}
+        renderRow  = {this.renderRow}
+        renderSectionHeader = {this.renderSectionHeader}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop:50,
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  sectionHeader: {
+    height: 15,
+    margin: 10,
+    color: '#546979',
+  },
+  section: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    padding: 6,
+    backgroundColor: '#2196F3'
+  },
+  text: {
+    color: 'white',
+    paddingHorizontal: 8,
+    fontSize: 16
+  },
+});
+
+module.exports = Activity;
---
+++ src/components/Activity/Row.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+class Row extends Component {
+  render() {
+    return (
+
+      // TODO: will be replaced with data from backend
+      <TouchableWithoutFeedback onPress={Actions.userProfile}>
+        <View style={styles.row}>
+          <Image style={styles.photo}
+                 source={{ uri: 'http://lorempixel.com/64/64/cats/' }}/>
+          <View style={styles.userInformation}>
+            <Text style={styles.name}>Hyunchan Kim</Text>
+          </View>
+          <TouchableHighlight style={styles.connectButton}>
+            <Text style={styles.connectButtonText}>Write an Email</Text>
+          </TouchableHighlight>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 3,
+    flexDirection: 'row',
+    backgroundColor: '#f7f7f9',
+    margin: 5,
+  },
+  photo: {
+    height: 80,
+    width: 80,
+    margin: 15,
+    borderRadius: 40,
+  },
+  imageSeperator: {
+    alignItems: 'center',
+    marginTop: 7.5,
+    marginBottom: 7.5,
+    borderWidth: 1,
+    borderColor: '#e3e3e3',
+  },
+  userInformation: {
+    flex: 7,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  },
+  name: {
+    justifyContent: 'center',
+    fontSize: 15,
+    fontWeight: 'normal',
+  },
+  job: {
+    fontSize: 13,
+    marginTop: 5,
+  },
+  education: {
+    fontSize: 13,
+    marginTop: 5,
+  },
+  connectButton: {
+    height: 40,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#1ecfe2',
+    borderRadius: 4,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+    padding: 10,
+  },
+  connectButtonText: {
+    fontSize: 12,
+    color: 'white',
+    alignSelf: 'center',
+  },
+});
+
+module.exports = Row;
